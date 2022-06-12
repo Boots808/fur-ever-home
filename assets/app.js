a8d76e8b9f756dfe4a0d4a906d8298b17cb90cb0
macm <mac.mitsch@comcast.net>
2022-06-12 12:58:29 -0500

    finished oauth token and app.js

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,23 @@
 import { useEffect, createContext, Component, useState } from "react";
 
 const AuthContext = createContext();
-const petFinderKey = "9pxAF85YH8bMvZlvyMCUsa0a8fKibR65rzf4Mu8IJizesNiKKN";
-const petFinderSecret = "XUuaPBK2gNLX0n40MiHrKH4ps3ow0UMJHhD38hTk";
+
 function MyApp({ Component, pageProps }) {
     const [accessToken, setAccessToken] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         const fetchAccessToken = async () => {
-            const params = new URLSearchParams();
-            params.append("grant_type", "client_credentials");
-            params.append("client_id", petFinderKey);
-            params.append("client_secret", petFinderSecret);
-            const petfinderRes = await fetch()
-        }
+           const res = await fetch("/api/oauth-token");
+           const json = await res.json();
+           setAccessToken (json.access_token);
+        };
+        fetchAccessToken();
     }, []);
     
     return(
-        <AuthContext.Provider>
+        <AuthContext.Provider value={(accessToken)}>
         <Component {...pageProps} />
         </AuthContext.Provider>
-};
+    );
+}
 export default MyApp;
