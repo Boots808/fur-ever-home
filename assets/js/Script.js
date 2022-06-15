ffedf916a14f1aa078bfd2a0ca8c1a2b1647f34f
macm <mac.mitsch@comcast.net>
2022-06-14 21:03:03 -0500

    base api

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,13 +1,22 @@
-/*const options = {
+var dogBreeds = document.getElementById("dog-search").value
+var dogInput = document.getElementById("dog-input")
+
+function getDogInfo(){
+
+var options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'c2e0789a8bmshe420e2ebff8e5b9p1b89a1jsn65f0fb090ec1',
 		'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
 	}
 };
-
-fetch('https://dog-breeds2.p.rapidapi.com/dog_breeds', options)
+    fetch(`https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/${dogBreeds}`, options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-    console.log("pups");*/
+  console.log(dogBreeds.value)
+ }
+// dog_input.addEventListener("click", getdoginfo())
+
+
+// console.log(dog_breeds.textContent)
