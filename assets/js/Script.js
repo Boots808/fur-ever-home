dd5e1cffda18bac5cb372946353ee5e019069b8f
macm <mac.mitsch@comcast.net>
2022-06-14 22:10:16 -0500

    working API just ad img. look up how to add a src img through javascript

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,22 +1,32 @@
-var dogBreeds = document.getElementById("dog-search").value
+var dogBreeds = document.getElementById("dog-search")
 var dogInput = document.getElementById("dog-input")
+var dogInfo = document.getElementById("dog-information")
+
+
+
 
 function getDogInfo(){
+	
+	let dog = dogBreeds.value
+	let dogInfoDiv = document.createElement('div')
+console.log(dog)
 
-var options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c2e0789a8bmshe420e2ebff8e5b9p1b89a1jsn65f0fb090ec1',
-		'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
-	}
-};
-    fetch(`https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/${dogBreeds}`, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-  console.log(dogBreeds.value)
- }
-// dog_input.addEventListener("click", getdoginfo())
-
-
-// console.log(dog_breeds.textContent)
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'c2e0789a8bmshe420e2ebff8e5b9p1b89a1jsn65f0fb090ec1',
+			'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
+		}
+	};
+	
+	fetch(`https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/${dog}`, options)
+	
+		.then(response => response.json())
+		.then(data =>{
+			console.log(data)
+			 dogInfo.innerText = data[0].breed
+        }
+			)
+        
+		.catch(err => console.error(err));
+}
