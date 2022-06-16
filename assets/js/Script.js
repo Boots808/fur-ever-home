f6da475a19b23e835562f874335c7d1f1cd479fd
macm <mac.mitsch@comcast.net>
2022-06-15 23:16:30 -0500

    added doge description having issues with it posting

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,39 +1,7 @@
-/*var dogBreeds = document.getElementById("dog-search")
-var dogInput = document.getElementById("dog-input")
-var dogInfo = document.getElementById("dog-information")
-var oImg = document.createElement("img")
 
-
-
-
-function getDogInfo(){
-	
-	let dog = dogBreeds.value
-	let dogInfoDiv = document.createElement('div')
-console.log(dog)
-
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'c2e0789a8bmshe420e2ebff8e5b9p1b89a1jsn65f0fb090ec1',
-			'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
-		}
-	};
-	
-	fetch(`https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/${dog}`, options)
-	
-		.then(response => response.json())
-		.then(data =>{
-			console.log(data)
-			 dogInfo.innerText = data[0].breed
-            
-        }
-			)
-        
-		.catch(err => console.error(err));
-}*/
 console.log("Doge")
 
+// Base API link
 const BASE_API_URL = 'https://api.thedogapi.com/v1'
 
 const fetchDogeBreeds = async () => {
@@ -42,7 +10,7 @@ const fetchDogeBreeds = async () => {
    const dogBreeds = await response.json();
    populateDoge(dogBreeds);
 }
-
+// Breed selection from dropdown
 const populateDoge = (breeds) => {
     const select = document.querySelector('.breed-select');
     const breedOptions = breeds.map(breed =>{
@@ -58,8 +26,53 @@ const populateDoge = (breeds) => {
     
     })
 }
+// Populated image for breeds
 const fillDogeImage = (imageUrl) => {
     document.querySelector('#Doge-Image').setAttribute('src', imageUrl)
+}
+
+// description list
+const createDescriptionEntry =({label, value}) => {
+    const descriptionTerm = document.createElement('dt');
+    descriptionTerm.textContent = label;
+    const descriptionValue = document.createElement('dd');
+    descriptionValue.textContent = value;
+    document.querySelector('#doge-description');
+    parentElement.appendChild(descriptionTerm);
+    parentElement.appendChild(descriptionValue);
+
+}
+// description section
+const fillDogeDescription = ({bredfor, name, temperament, lifeSpan, Origin,height,weight }) => {
+    clearDogeDescription
+createDescriptionEntry({
+    label: 'Name',
+    value: name
+})
+createDescriptionEntry({
+    label:'Bred for',
+    value: bredfor
+})
+createDescriptionEntry({
+    label:'Temperement',
+    value: temperament
+})
+createDescriptionEntry({
+    label:'Life Span',
+    value: lifeSpan
+})
+createDescriptionEntry({
+   label:'Origin',
+   value: Origin
+})
+createDescriptionEntry({
+    label:'Height [cm]',
+    value: height.metric
+})
+createDescriptionEntry({
+    label:'Weight [kg]',
+    value: weight.metric
+})
 }
 
 const getDogeByBreed = async (breedId) => {
@@ -67,6 +80,7 @@ const getDogeByBreed = async (breedId) => {
 const [ data ] = await fetch (BASE_API_URL + '/images/search?include_breed=1&breed_id=' + breedId ).then((data) => data.json())
 const {url: imageUrl, breeds} = data;
 fillDogeImage(imageUrl);
+fillDogeDescription(breeds[0]);
 }
 const changeDoggo = () => {
     console.log(event.target.value);
